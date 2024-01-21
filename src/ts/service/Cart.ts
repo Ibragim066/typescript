3641e3bdbdf3b229eb8dea5b79cc0c29dc09ce0a
Ibragim <gamemega.ao@gmail.com>
2024-01-21 12:46:07 +0300

    purchase amount

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -11,4 +11,16 @@ export default class Cart {
   get items(): Buyable[] {
     return [...this._items];
   }
+
+  countTotal(): number {
+    return this._items.reduce((sum, item) => sum + item.price * item.count, 0);
+  }
+
+  countTotalWithDiscount(discount: number): number {
+    return this.countTotal() * (1 - discount / 100);
+  }
+
+  removeCart(id: number): void {
+    this._items = this._items.filter((item: Buyable) => item.id !== id);
+  }
 }
